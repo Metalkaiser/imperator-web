5c8fc810aa0b1486396c2647f5257fb49cdf6199
MetalKaiser <metalkaiserpolanco@gmail.com>
2022-10-31 14:23:15 -0400

    first commit

---
+++ ReactJS_FrontEnd/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function Welcome(props) {
+  return <h1>Hello, {props.name}</h1>;
+}
+
+function App() {
+  return (
+    <Welcome name="Juan" />
+  );
+}
+
+export default App;
